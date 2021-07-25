6ffa847e12d03d111b2594d8b82309a80a27a5f4
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-07-25 16:28:29 +0300

    add new folders and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import './App.scss';
-import Customers from './components/Customers/Customers';
-import Users from './components/Users/Users';
+import Client from './client/Client.jsx';
+import Admin from './admin/Admin';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Customers/>
-      <Users/>
-    </div>
+    <>
+      <Router>
+        <Switch>
+         <Route path="/admin"><Admin/></Route>
+          <Route path="/"><Client/></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
