c71224d59b383366c7fdd14c4bfaa1d072f37dba
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-07-25 16:40:31 +0300

    add header/footer

---
+++ frontend/src/client/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Footer</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/client/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+function Header() {
+  return (
+    <header>
+      <p>Header</p>
+    </header>
+  );
+}
+
+export default Header;
