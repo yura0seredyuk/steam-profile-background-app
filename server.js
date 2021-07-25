ddf57093338a14bfebe7bec2123b1ad53ad18940
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-07-25 21:09:22 +0300

    connect mongo db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use('/users/', require('./routes/usersRoute'));
 
@@ -10,6 +11,19 @@ app.get('/api/customers', (req, res) => {
   ]; 
 
   res.json(customers);
+})
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.1b6uf.mongodb.net/steam-profile-background-app?retryWrites=true&w=majority', {
+  useNewUrlParser: "true",
+  useUnifiedTopology: "true"
+})
+
+mongoose.connection.on('error', err => {
+  console.log('err', err);
+})
+
+mongoose.connection.on("connected", (err, res) => {
+  console.log("mongoose is connected")
 })
 
 const PORT = process.env.PORT || 4000;
